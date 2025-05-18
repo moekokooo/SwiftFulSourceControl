eff94d97cc0019a9093ab01250d080f6a166dfcd
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-18 20:42:14 +0630

    Initial Commit

---
+++ SwiftFulSourceControl/SwiftFulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFulSourceControlApp.swift
+//  SwiftFulSourceControl
+//
+//  Created by Moe Ko Ko Oo on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
