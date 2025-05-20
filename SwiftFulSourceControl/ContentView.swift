e5b9497ebae89ea493cd89bebbf7fed3f60eb879
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-20 21:04:30 +0630

    Add Click Me Button

--- SwiftFulSourceControl/ContentView.swift
+++ SwiftFulSourceControl/ContentView.swift
@@ -15,7 +15,9 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("SwiftFul Thinking!")
             
-        
+            Button("Click Me!") {
+                
+            }
         }
         .padding()
     }
