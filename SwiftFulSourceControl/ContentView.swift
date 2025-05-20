0472d633ef782edca5ff771ec8f03656244e6b9c
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-20 21:30:34 +0630

    [Patch] Fix Click Me background color to red (was green)

--- SwiftFulSourceControl/ContentView.swift
+++ SwiftFulSourceControl/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             Button("Click Me!") {
                 
             }
-            .background(.green)
+            .background(.red)
         }
         .padding()
     }
