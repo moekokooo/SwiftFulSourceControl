0f84f9d93a3d147ead79070e5f96e351257063a6
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-20 21:09:26 +0630

    Add Home View

---
+++ SwiftFulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftFulSourceControl
+//
+//  Created by Moe Ko Ko Oo on 20/05/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home View 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
