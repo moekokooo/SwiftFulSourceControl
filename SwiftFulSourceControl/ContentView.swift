c003108e3df182508904e9fd5d548bd74da9eabd
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-22 14:33:49 +0630

    [Feature] Change subscribe now text ( was subscribe )

--- SwiftFulSourceControl/ContentView.swift
+++ SwiftFulSourceControl/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("SwiftFul Thinking!")
             
-            Button("Subscribe!") {
+            Button("Subscribe Now!") {
                 
             }
         }
