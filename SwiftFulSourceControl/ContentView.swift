dd52eb2613d0860dbc7b2231acb105c423cdb7f4
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-24 00:26:59 +0630

    [Feature] add new content view

--- SwiftFulSourceControl/ContentView.swift
+++ SwiftFulSourceControl/ContentView.swift
@@ -9,20 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(0..<20) { _ in
-                    Image(systemName: "globe")
-                        .font(.largeTitle)
-                        .foregroundStyle(.red)
-                    Text("New Title!")
-                    Button("Click me") {
-                }
-        
+        VStack {
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
+                .foregroundStyle(.red)
             
+            Text("Hello, World!")
+            Button("Subscribe") {
                 
             }
-        
+        }
     }
 }
 
