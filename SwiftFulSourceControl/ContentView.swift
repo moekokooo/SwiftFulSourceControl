c8550c0a7de5594b25c3ffc3839bfc30f7ca0325
Moe Ko Ko Oo <moekokooo1994@gmail.com>
2025-05-24 00:28:34 +0630

    [Feature] add scroll view

--- SwiftFulSourceControl/ContentView.swift
+++ SwiftFulSourceControl/ContentView.swift
@@ -10,13 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "heart.fill")
-                .font(.largeTitle)
-                .foregroundStyle(.red)
-            
-            Text("Hello, World!")
-            Button("Subscribe") {
-                
+            ScrollView {
+                VStack {
+                    ForEach(0..<20) { _ in
+                        Text("Hello, World!")
+                    }
+                }
             }
         }
     }
